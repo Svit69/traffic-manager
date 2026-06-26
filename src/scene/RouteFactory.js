c8e14341d2621a85a707afb8e013d88329223a19
Svit69 <ol.svitenkov@gmail.com>
2026-06-26 14:35:38 +0500

    Move horizontal vehicle lane up

--- src/scene/RouteFactory.js
+++ src/scene/RouteFactory.js
@@ -15,11 +15,11 @@ export class RouteFactory {
   }
 
   createHorizontalRoute(frame) {
-    const carWidth = frame.width * 0.212;
+    const carWidth = frame.width * 0.191;
     return {
       frame,
       x: this.#resolveX(frame, -0.16, 0),
-      y: this.#resolveY(frame, 0.595) - 50 * frame.scale,
+      y: this.#resolveY(frame, 0.595) - 80 * frame.scale,
       exitX: this.#resolveX(frame, 1.16, 0),
       carHeight: carWidth * 0.58,
       carWidth,
